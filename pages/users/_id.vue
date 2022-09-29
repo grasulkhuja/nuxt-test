d19a4d697fba497ff5828e612311d84e65283230
grasulkhuja <rasulkhujaganiev@gmail.com>
2022-09-29 12:50:56 +0500

    Start point

---
+++ pages/users/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="user" class="container px-5 py-5">
+    <ul>
+      <li v-for="[key, value] in Object.entries(user)" :key="key">
+        {{ key }}: {{ value }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserItem',
+  data() {
+    return {
+      user: null,
+    }
+  },
+  created() {
+    this.getUserById()
+  },
+  methods: {
+    async getUserById() {
+      const { data } = await this.$axios.get(
+        'https://jsonplaceholder.typicode.com/users/' + this.$route.params.id
+      )
+      this.user = data
+    },
+  },
+}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container px-5 py-5">
+    <ul v-if="users.length">
+      <li v-for="user in users" :key="user.id">
+        <NuxtLink :to="'users' + user.id">{{ user.name }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersIndex',
+  data() {
+    return {
+      users: [],
+    }
+  },
+  created() {
+    this.getUsers()
+  },
+  methods: {
+    async getUsers() {
+      const { data } = await this.$axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+      )
+      this.users = data
+    },
+  },
+}
+</script>
